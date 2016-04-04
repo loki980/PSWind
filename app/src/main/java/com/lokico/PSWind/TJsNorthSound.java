749843911765b701ede92073cbd54c83c435c196
jheckathorn <jaheckathorn@gmail.com>
2016-04-04 16:23:16 -0700

    Implemented TJs North Sound. First cut. Basic functionality.

--- app/src/main/java/com/lokico/PSWind/TJsNorthSound.java
+++ app/src/main/java/com/lokico/PSWind/TJsNorthSound.java
@@ -1,16 +1,25 @@
 package com.lokico.PSWind;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class TJsNorthSound extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tjs_north_sound);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.getSettings().setAppCacheEnabled(false);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setBackgroundColor(Color.parseColor("#699ccf"));
+        myWebView.loadUrl("http://pskite.org/looptest/android_tj_northsound.php");
     }
 
     static boolean isActivityImplemented() {
-        return false;
+        return true;
     }
 }
