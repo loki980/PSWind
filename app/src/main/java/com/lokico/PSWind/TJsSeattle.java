9abe3fcd1de211fd0409595e86cd4c2763e9a8da
jheckathorn <jaheckathorn@gmail.com>
2016-04-04 15:42:40 -0700

    Implemented TJs Seattle (minimum functionality at this time)

--- app/src/main/java/com/lokico/PSWind/TJsSeattle.java
+++ app/src/main/java/com/lokico/PSWind/TJsSeattle.java
@@ -1,7 +1,10 @@
 package com.lokico.PSWind;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class TJsSeattle extends AppCompatActivity {
 
@@ -9,9 +12,15 @@ public class TJsSeattle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tjs_seattle);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.getSettings().setAppCacheEnabled(false);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setBackgroundColor(Color.parseColor("#699ccf"));
+        myWebView.loadUrl("http://pskite.org/looptest/android_tj_seattle.php");
     }
 
     static boolean isActivityImplemented() {
-        return false;
+        return true;
     }
 }
