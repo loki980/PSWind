a89b0aa83510112d88e07ad58a4b8b3b9b8ce43c
Jeff Heckathorn <jaheckathorn@gmail.com>
2016-05-04 14:05:48 -0700

    Implemented basic map functionality as part of the OmniMap activity.

--- app/src/main/java/com/lokico/PSWind/OmniMap.java
+++ app/src/main/java/com/lokico/PSWind/OmniMap.java
@@ -1,16 +1,50 @@
 package com.lokico.PSWind;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * The interactive sensor map <code>Activity</code>.
  */
-public class OmniMap extends AppCompatActivity {
+public class OmniMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_omni_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.display_omni_map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker at Jetty and move the camera
+        LatLng jetty = new LatLng(48.008154, -122.229781);
+        mMap.addMarker(new MarkerOptions().position(jetty).title("Marker in Jetty"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(jetty));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo((float) 12.0));
     }
 
     /**
