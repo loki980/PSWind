e06a02cc9724ef5c9aec9a502df3f43bd615f9cf
Jeff Heckathorn <jaheckathorn@gmail.com>
2016-05-11 13:33:36 -0700

    Can now show multiple wind sensors on the map that indicate wind strength and direction. Numerical values for wind speed not shown yet.

--- app/src/main/java/com/lokico/PSWind/OmniMap.java
+++ app/src/main/java/com/lokico/PSWind/OmniMap.java
@@ -1,5 +1,7 @@
 package com.lokico.PSWind;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -11,12 +13,16 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The interactive sensor map <code>Activity</code>.
  */
 public class OmniMap extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private List<WindSensor> mWindSensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +52,14 @@ public class OmniMap extends FragmentActivity implements OnMapReadyCallback {
         // Add a marker at Jetty and move the camera
         LatLng jetty = new LatLng(48.008154, -122.229781);
         mMap.addMarker(new MarkerOptions().position(jetty).title("Marker in Jetty"));
-        addMarkers(mMap);
+
+        // TODO Move to background
+        // Parse the response
+        mWindSensorList = new ArrayList<WindSensor>();
+        parseRawSensorData("", mWindSensorList);
+
+        // Add the parsed sensor data
+        addMarkers(this, mMap,mWindSensorList);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(jetty));
         //addMarkers(mMap);
         mMap.moveCamera(CameraUpdateFactory.zoomTo((float) 10.0));
@@ -65,14 +78,45 @@ public class OmniMap extends FragmentActivity implements OnMapReadyCallback {
         return null;
     }
 
-    static private void parseRawSensorData(String rawSensorData) {
+    static private void parseRawSensorData(String rawSensorData, List<WindSensor> windSensors) {
+        // TODO Test data
+        /*
+        <markers>
+<marker id="WOTW07" label="Hat Island" lat="48.0194" lng="-122.334" wind="1" gust="3" angle="225" timestamp="1462988635"/>
+<marker id="WA010" label="Locust Beach" lat="48.7767" lng="-122.562" wind="3" gust="5" angle="337" timestamp="1462988340"/>
+<marker id="PBFW1" label="Padilla Bay" lat="48.4639" lng="-122.468" wind="2" gust="4" angle="315" timestamp="1462984200"/>
+<marker id="KNUW" label="Whidbey Island" lat="48.3492" lng="-122.651" wind="7" gust="0" angle="135" timestamp="1462985760"/>
+<marker id="WA001" label="Jetty Island" lat="48.0035" lng="-122.228" wind="6" gust="3" angle="90" timestamp="1409602200"/>
 
+        *
+         */
+        //WindSensor windSensor = new WindSensor(lat, lon, title, dir, speed)
+        windSensors.add(new WindSensor((float) 48.0194,(float)-122.334, "Hat Island", 225, 1));
+        windSensors.add(new WindSensor((float) 48.7767,(float)-122.562, "Locust Beach", 337, 3));
+        windSensors.add(new WindSensor((float) 48.4639,(float)-122.468, "Padilla Bay", 315, 4));
+        windSensors.add(new WindSensor((float) 48.3492,(float)-122.651, "Whidbey Island", 135, 7));
+        windSensors.add(new WindSensor((float) 48.0035,(float)-122.228, "Jetty Island", 90, 6));
     }
 
-    static private void addMarkers(GoogleMap map) {
+    static private void addMarkers(Context context, GoogleMap map, List<WindSensor> windSensorList) {
         //lat="48.0194" lng="-122.334
-        LatLng jetty = new LatLng(48.0194, -122.334);
-        map.addMarker(new MarkerOptions().position(jetty).title("Hat Island")
-        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_high_wind)));
+        //LatLng jetty = new LatLng(48.0194, -122.334);
+        //map.addMarker(new MarkerOptions().position(jetty).title("Hat Island")
+        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_high_wind)));
+        if (windSensorList != null && !windSensorList.isEmpty()) {
+            for (WindSensor windSensor : windSensorList) {
+                // Add sensors to map
+                String resIdName = windSensor.getBaseIconName();
+                String packageName = context.getPackageName();
+                Resources resources = context.getResources();
+                map.addMarker(new MarkerOptions()
+                        .position(windSensor.getLatLng())
+                        .title(windSensor.getTitle())
+                        .rotation(windSensor.getDirection())
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(resources.getIdentifier(
+                                        resIdName, "drawable", packageName))));
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/lokico/PSWind/WindSensor.java
@@ -0,0 +1,51 @@
+package com.lokico.PSWind;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class WindSensor {
+    LatLng mLatLng;
+    String mTitle;
+    String mBaseIconName;
+    float mDirection;
+    float mSpeed;
+    String mSpeedIconName;
+
+    // TODO Consider using a builder pattern
+    public WindSensor(float lat, float lon, String title, float direction,
+                      float speed) {
+        mLatLng = new LatLng(lat, lon);
+        mTitle = title;
+        mDirection = direction;
+        mSpeed = speed;
+
+        // Select icons to display itself
+        //mBaseIconName = "drawable/";
+        mBaseIconName = "";
+        if (mSpeed > 27) {
+                        /* Prefix for red arrow */
+            mBaseIconName += "marker_high_wind";
+        } else if (mSpeed > 19) {
+                        /* Prefix for orange arrow */
+            mBaseIconName += "marker_medium_wind";
+        } else if (mSpeed > 13) {
+                        /* Prefix for green arrow */
+            mBaseIconName += "marker_light_wind";
+        } else {
+                        /* Prefix for grey arrow */
+            mBaseIconName += "marker_no_wind";
+        }
+
+        mSpeedIconName = "drawable/windspeed";
+        if (mSpeed >= 0 && mSpeed < 100) {
+            mSpeedIconName = mSpeedIconName + Integer.toString((int)mSpeed);
+        }
+    }
+
+    // Getters
+    LatLng getLatLng() {return mLatLng;}
+    String getTitle() {return mTitle;}
+    String getBaseIconName() {return mBaseIconName;}
+    String getSpeedIconName() {return mSpeedIconName;}
+    float getDirection() {return mDirection;}
+    float getSpeed() {return mSpeed;}
+}
